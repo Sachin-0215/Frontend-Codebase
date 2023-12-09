a7b864a793acc167b7a96925dbd90f3d1efd5024
Sachin Gupta <sgupta15120@gmail.com>
2023-12-09 23:37:55 +0530

    modification

--- JavaScript/Fourth Pillar Of JS/async_sync_demo.js
+++ JavaScript/Fourth Pillar Of JS/async_sync_demo.js
@@ -1,27 +1,53 @@
 function timeTakenByLoop() {
     console.log("Loops starts");
-
-    for (let i = 0; i < 1000000; i++){
+    for (let i = 0; i < 100000000; i++){
         //do some task
     }
-
     console.log("Loops ends here");
+}
+
+function timeTakenByRuntimeTimeoutFeature0() {
+    console.log("Timer starts0")
+    setTimeout(function f() {
+        console.log("Runing till 200ms")
+        for (let i = 0; i < 100000000; i++){
+            //do some task
+        }
+        console.log("Timer ends here0");
+    }, 200);//200 ms
 
 }
 
-function timeTakenByRuntimeTimeoutFeature() {
-    console.log("Timer starts")
+function timeTakenByRuntimeTimeoutFeature1() {
+    console.log("Timer starts1")
+    setTimeout(function f() {
+        console.log("Runing till 0ms")
+        for (let i = 0; i < 100000000; i++){
+            //do some task
+        }
+        console.log("Timer ends here1");
+    }, 0);//0s timer
+
+}
+
+function timeTakenByRuntimeTimeoutFeature2() {
+    console.log("Timer starts2")
     setTimeout(function f() {
         console.log("Runing till 5000ms")
-    }, 5000);
-    console.log("Timer ends here");
+        for (let i = 0; i < 100000000; i++){
+            //do some task
+        }
+        console.log("Timer ends here2");
+    }, 5000);//5 sec timer
 
 }
 
 console.log("Hii this is Your JavaScript Language");
 
 timeTakenByLoop();
-timeTakenByRuntimeTimeoutFeature();
+timeTakenByRuntimeTimeoutFeature0();
+timeTakenByRuntimeTimeoutFeature1();
+timeTakenByRuntimeTimeoutFeature2();
 timeTakenByLoop();
 
-console.log("Bye");
+console.log("Bye this is your js language sign of");
