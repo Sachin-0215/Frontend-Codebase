90fdb5c7d4ca1cf14542209d51acac6807f9f8bb
Sachin Gupta <sgupta15120@gmail.com>
2023-12-11 23:59:10 +0530

    Day 14

--- JavaScript/Fourth Pillar Of JS/setInterval.js
+++ JavaScript/Fourth Pillar Of JS/setInterval.js
@@ -6,3 +6,8 @@ setInterval(function () {
 
 //this will run and print at every 1000ms
 
+setInterval(function () {
+    console.log("another second one at 500ms")
+}, 500);
+
+//this will run and print at every 1000ms then nextOne print at every 500ms
