6d59e1831625416598f56fefef5b2e90cef0a6e6
Sachin Gupta <sgupta15120@gmail.com>
2023-12-12 20:45:42 +0530

    Day 15 work

--- JavaScript/Fourth Pillar Of JS/setInterval.js
+++ JavaScript/Fourth Pillar Of JS/setInterval.js
@@ -1,13 +1,29 @@
 // It is an interesting API.Set Intervals takes a callback function
 
-setInterval(function () {
-    console.log("another one at 1000s");
-}, 1000)
+// setInterval(function () {
+//     console.log("another one at 1000s");
+// }, 1000)
 
-//this will run and print at every 1000ms
+// //this will run and print at every 1000ms
 
-setInterval(function () {
-    console.log("another second one at 500ms")
-}, 500);
+// setInterval(function () {
+//     console.log("another second one at 500ms")
+// }, 500);
 
 //this will run and print at every 1000ms then nextOne print at every 500ms
+
+
+//to stop the interval we can store their id into a variable and then pass that id in clearInterval to stop the interval
+
+let x = setInterval(function () {
+    console.log("another one at 1000ms");
+},1000)
+
+let v = setInterval(function () {
+    console.log("another interval at 500ms");
+}, 500);
+
+clearInterval(x);
+clearInterval(v);
+
+//not running because using id , we are using clearInterval
