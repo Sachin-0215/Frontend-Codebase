5e110e6b15b0e97e392da6244221991782db1c7e
Sachin Gupta <sgupta15120@gmail.com>
2023-12-13 19:58:17 +0530

    more about promises

---
+++ JavaScript/Fourth Pillar Of JS/Advance Part/consuming_promise.js
@@ -0,0 +1,38 @@
+// setTimeout(function exec() {
+//     console.log("done")
+// }, 3000);
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function createPromiseWithTimeOUT() {
+    return new Promise(function executor(resolve, reject){
+        console.log("Entering the executor callback in the promise constructor");
+        setTimeout(function () {
+            let num = getRandomInt(10);
+            if (num % 2 == 0) {
+                //if the random number is even we fullfill 
+                resolve(num);
+            }
+            else {
+                //if random no. is odd we reject
+                reject(num);
+            }
+        }, 10000);
+        console.log("Exitting the executor callback in the promise constructor");
+    })
+}
+console.log("Starting");
+const P = createPromiseWithTimeOUT();
+console.log("We are now waiting for the promise to complete");
+console.log("Currently my promise object is like....", P);
+P.
+    then(
+    function fullfillHandler(value) { console.log("Inside fullfill handler with value", value); },
+    function rejectionHandler(value) { console.log("Inside rejection handler with value", value); }
+);
+
+console.log(P);
+console.log(typeof P);
