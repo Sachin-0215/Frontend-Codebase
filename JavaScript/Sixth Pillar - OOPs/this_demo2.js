9891c3bc74958132c95d55d4bc48a4995128890b
Sachin Gupta <sgupta15120@gmail.com>
2023-12-23 23:58:57 +0530

    modify

--- JavaScript/Sixth Pillar - OOPs/this_demo2.js
+++ JavaScript/Sixth Pillar - OOPs/this_demo2.js
@@ -1,3 +1,34 @@
-let array_of_product = [{
-    
-}]
+let array_of_product = {
+    arr: [
+        {
+            name: "Macbook M2",
+            price: "200000",
+            description: "The new MacBook Edition",
+            rating: 4.9,
+
+            display() {
+                console.log("first display ", this);
+            },
+            getCategory: function print() {
+                console.log(this.category);
+            }
+        }
+            ,
+        {
+            name: "Google Pixel 7",
+            price: "91000",
+            description: "The new Google smartPhone",
+            rating: 4.8,
+
+            display() {
+                console.log("second display ", this);
+            },
+            getCategory: function print() {
+                console.log(this.category);
+            }
+        }
+    ],
+    category:"Electronics"
+}
+
+array_of_product.arr[0].getCategory();
