b86e25a905cb8fac5569fdc8edd66ad525695ca8
Sachin Gupta <sgupta15120@gmail.com>
2023-12-25 18:03:07 +0530

    code 1

---
+++ JavaScript/Sixth Pillar - OOPs/func_constructor.js
@@ -0,0 +1,18 @@
+function Product(n,p,d){
+    this.name = n;
+    this.price = p;
+    this.description = d; 
+    
+    this.display = function() {
+        console.log(this);
+    }
+
+    //return 10;//this primitive return will not affect anything
+    return this;
+    // return {};//empty objects
+}
+
+
+const p = new Product("Power Bank", 1000, "Brand new power bank");
+console.log(p);
+p.display();
