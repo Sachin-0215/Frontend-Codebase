472bf901796d2091d1edb27263387624b90e9e08
Sachin Gupta <sgupta15120@gmail.com>
2023-12-28 22:07:35 +0530

    modify for learning props

--- React/amazon/src/App.js
+++ React/amazon/src/App.js
@@ -1,13 +1,15 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
-import Project1 from './Project';
+// import Project1 from './Project1';
+import Data from './Data'
 
 function App() {
   return (
-    <div >
-      <Project1 name="XYZ" age="10"/>
+    <div>
+      <Data firstName="Sachin" lastName="Gupta" rollNo = "1046" age="22" skills= "Full Stack Web Developement" />
     </div>
-  );
+  )
 }
 
 export default App;
