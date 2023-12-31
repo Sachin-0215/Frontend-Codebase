ca5b7bc0424a5b6ddfe5abf2e777975e786e9af6
Sachin Gupta <sgupta15120@gmail.com>
2023-12-31 23:13:23 +0530

    note and code s

---
+++ JavaScript/DOM/index.js
@@ -0,0 +1,10 @@
+console.log("End from index.js file");
+// for (let i = 0; i < 100000000000; i++) { }
+// console.log("End");
+
+//reference link : https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#async
+
+
+setTimeout(function f() {
+    document.title = "Hello World";
+}, 3000);
