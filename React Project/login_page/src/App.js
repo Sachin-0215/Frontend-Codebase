076a191c415b58f9257d3a2c9db6628535be1903
Sachin Gupta <sgupta15120@gmail.com>
2024-01-04 22:52:40 +0530

    modified

--- React Project/login_page/src/App.js
+++ React Project/login_page/src/App.js
@@ -5,13 +5,11 @@ function App() {
   return (
     <div className="App">
       <div className="loginContainer">
-          <h1>Welcome Back</h1>
-
+          <h1>Sign in </h1>
         <div className="input-container">
             <label>Username  </label>
             <input type="text" required />
         </div>
-
         <div className="input-container">
             <label>Password </label>
             <input type="password" required />
@@ -23,16 +21,13 @@ function App() {
         <button className="loginBut">
           <p>Login</p>
         </button>
-
         <button className="siG">
           <img
             src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
             alt="Trees" height="30" />
           <p>Sign in with Google</p>
         </button>
-
       </div>
-  
     </div>
   );
 }
--- React Project/simple_project/src/index.js
+++ React Project/simple_project/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Incr_Decr from './Incr_Decr';
+import './Incr_Decr.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App />
+     */}
+    <Incr_Decr/>
   </React.StrictMode>
 );
 
