a6e57cbd8cd7d0954dff76a3f7b654a10d40ebd5
Sachin Gupta <sgupta15120@gmail.com>
2024-01-13 17:40:10 +0530

    files modified because of adding new functionality

--- React/amazon-clone/src/App.js
+++ React/amazon-clone/src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './Components';
+import { HomePage , NavBar,Checkout,SearchResults,ProductPage} from './Components';
 
 const App = () => {
   return (
         <BrowserRouter>
+            <NavBar/>
             <Routes>
               <Route exact path="/" element={<HomePage/> } />  
+              <Route exact path="/search" element={<SearchResults/> } />  
+              <Route exact path="/product/:id" element={<ProductPage/> } />  
+              <Route exact path="/checkout" element={<Checkout/> } />  
             </Routes>  
         </BrowserRouter>
   )
