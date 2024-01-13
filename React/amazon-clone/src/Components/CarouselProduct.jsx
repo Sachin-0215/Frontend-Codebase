ef81d87bee6572924e5339adaa66e0fa0403d87c
Sachin Gupta <sgupta15120@gmail.com>
2024-01-13 17:37:38 +0530

    Carousel product part done

--- React/amazon-clone/src/Components/CarouselProduct.jsx
+++ React/amazon-clone/src/Components/CarouselProduct.jsx
@@ -1,5 +1,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation} from 'swiper/modules';
+import { Link } from 'react-router-dom';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -18,7 +19,9 @@ const CarouselProduct = () => {
               {
                   Array.from({ length: 9 }, (_, i) => 
                       <SwiperSlide key={i}>
-                          <img src={`../images/product_${i}_small.jpg`}/>
+                          <Link to={`/product/${i}`}>
+                            <img src={`../images/product_${i}_small.jpg`}/>  
+                          </Link>
                       </SwiperSlide>
                       
                   )
