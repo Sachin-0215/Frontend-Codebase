804b967f8d2e29eb0bdc1fa58de9c252b01b82ee
Sachin Gupta <sgupta15120@gmail.com>
2024-01-13 17:35:05 +0530

    Product information , rating and badges created

---
+++ React/amazon-clone/src/Components/ProductPage.jsx
@@ -0,0 +1,65 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { ProductDetails } from './'
+import { GB_CURRENCY } from '../utils/Constants';
+import { callAPI } from '../utils/CallApi';
+
+const ProductPage = () => {
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+
+    const getProduct = () => {
+        callAPI(`data/products.json`)
+            .then((productResults) => {
+                setProduct(productResults[id]);
+            });
+    }
+
+    useEffect(() => {
+        getProduct();
+    }, []);
+
+    if (!product?.title) return <h1>Loading Product ...</h1>
+    return (product &&
+        <div className='h-screen bg-amazonclone-background'>
+            <div className='min-w-[1000px] max-w-[1500px] m-auto p-4 '>
+                <div className='grid grid-cols-10 gap-2'>
+                    {/* left   */}
+                    <div className='col-span-3 p-8 rounded bg-white m-auto'>
+                        <img src={`${product.image}`} />
+                    </div>
+                    {/* middle */}
+                    <div className='col-span-5 p-4 rounded bg-white divide-y divide-gray-400'>
+                        <div className='mb-3'>
+                            <ProductDetails product={product} ratings={true} />
+                        </div>
+                        <div className='text-base xl:text-lg mt-3'>
+                            {product.description}
+                        </div>
+                    </div>
+                    {/* right */}
+                    <div className='col-span-2 p-4 rounded bg-white'>
+                        <div className='text-xl xl:text-2xl text-red-600 text-right font-semibold'>{GB_CURRENCY.format(product.price)}</div>
+                        <div className='text-base xl:text-lg text-gray-500 text-right font-semibold'>RRP: <span className='line-through'>{GB_CURRENCY.format(product.oldPrice)}</span></div>
+                        <div className='text-sm xl:text-base text-blue-500 font-semibold mt-3'>FREE Returns</div>
+                        <div className='text-sm xl:text-base text-blue-500 font-semibold mt-1'>FREE Delivery</div>
+                        <div className='text-base xl:text-lg text-green-600 font-semibold mt-1'>In Stock</div>
+                        <div className='text-base xl:text-lg mt-1'>Quantity:
+                            <select className='p-2 bg-white border rounded-md focus:border-indigo-600 '>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                            </select>
+                        </div>
+                        <button className='bg-yellow-400 w-full p-3 text-xs xl:text-sm rounded hover:bg-yellow-500 mt-3 '>Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default ProductPage
