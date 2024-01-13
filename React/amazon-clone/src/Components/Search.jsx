77ac159d7d0b9c64ba2906583f0341d984eda14c
Sachin Gupta <sgupta15120@gmail.com>
2024-01-13 20:51:36 +0530

    search functionality create

--- React/amazon-clone/src/Components/Search.jsx
+++ React/amazon-clone/src/Components/Search.jsx
@@ -1,10 +1,46 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import {useState , useEffect } from 'react';
+
+import { useNavigate,createSearchParams } from "react-router-dom";
+import { callAPI } from '../utils/CallApi';
 
 const Search = () => {
+
+  const [suggestions, setSuggestions] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("All");
+  const navigate = useNavigate();
+
+  const onHandleSubmit = (e) => { 
+    e.preventDefault();
+    navigate({
+      pathname: "search",
+      search: `${createSearchParams({
+        category: `${category}`,
+        searchTerm: `${searchTerm}`
+      })
+        }`,
+    });
+    setSearchTerm("");
+    setCategory("All");
+  }
+
+  const getSuggestions = () => {
+    callAPI(`data/suggestions.json`)
+      .then((suggestionResults) => {
+        setSuggestions(suggestionResults);
+      });
+  };
+  
+  useEffect(() => {
+      getSuggestions();
+  }, []);
+  
   return (
       <div className='w-[100%]'>
           <div className='flex items-center h-10 bg-amazonclone-yellow rounded'>
-                <select className='p-2 bg-gray-300 text-black border text-xs xl:text-sm'>
+                <select onChange={(e) => setCategory(e.target.value)}
+                    className='p-2 bg-gray-300 text-black border text-xs xl:text-sm'>
                     <option>All</option>
                     <option>Deals</option>
                     <option>Alexa Skills</option>
@@ -13,11 +49,31 @@ const Search = () => {
                     <option>Home</option>
                     <option>Mobiles</option>
                 </select>
-                <input className='flex grow items-center h-[100%] rounded-l text-black ' type='text'/>
-                <button className='w-[45px]'>
+                <input className='flex grow items-center h-[100%] rounded-l text-black ' type='text' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)}/>
+                <button onClick={onHandleSubmit} className='w-[45px]'>
                     <MagnifyingGlassIcon className='h-[27px] m-auto stroke-slate-900'/>      
                 </button>
           </div>
+      {
+        suggestions &&
+        <div className='bg-white text-black w-full z-40 absolute'>
+          {
+            suggestions.filter((suggestion) => {
+              const currentSearchTerm = searchTerm.toLowerCase();
+              const title = suggestion.title.toLowerCase();
+              return (
+                currentSearchTerm && title.startsWith(currentSearchTerm) &&
+                title !== currentSearchTerm
+              );
+            })
+              .slice(0, 10)
+              .map((suggestion) => {
+                    <div key={suggestion.id} onClick={() => setSearchTerm(suggestion.title)}>
+                        {suggestion.title}
+                    </div> 
+              })}
+         </div>
+      }
     </div>
   )
 }
--- React/amazon-clone/src/Components/SearchResults.jsx
+++ React/amazon-clone/src/Components/SearchResults.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { callAPI } from '../utils/CallApi';
 
 const SearchResults = () => {
+  const [searchParams] = useSearchParams();
+  const [products, setProducts] = useState(null);
+  const getSearchResults = () => {
+    const searchTerm = searchParams.get("searchTerm");
+    const category = searchParams.get("category");
+
+    callAPI(`data/search.json`)
+      .then((searchResults) => {
+        const categoryResults = searchResults(category);
+        if (searchTerm) {
+          const results = categoryResults.filter(product => product.title.toLowerCase().includes(searchTerm.toLowerCase()));
+          setProducts(results);
+        }
+        else {
+          setProducts(categoryResults);
+        }
+      });
+  };
+
+  useEffect(() => {
+    getSearchResults();  
+  }, [searchParams]);
+
   return (
-    <div>
-       Search Results
+    <div className='min-w-[1200px] max-w-[1300px] m-auto '>
+      {
+        products && products.map((product,key) => {
+          return (
+            <div key={key}>{product.title}</div>
+          )
+        })
+      }
     </div>
   )
 }
