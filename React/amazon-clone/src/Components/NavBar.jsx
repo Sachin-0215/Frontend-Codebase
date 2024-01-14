8ba6689c6f91c85fdefa07a0e5eb679e5b6d2520
Sachin Gupta <sgupta15120@gmail.com>
2024-01-14 16:51:09 +0530

    navbar updated

--- React/amazon-clone/src/Components/NavBar.jsx
+++ React/amazon-clone/src/Components/NavBar.jsx
@@ -1,9 +1,10 @@
 import { ShoppingCartIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom';
-
+import { UseSelector, useSelector } from 'react-redux';
 import {Search } from './'
 
 const NavBar = () => {
+    const cart = useSelector((state) => state.cart.productsNumber);
     return (
       <header className='min-w-[1000px]'>
             <div className='flex bg-amazonclone text-white h-[60px]'>
@@ -36,7 +37,12 @@ const NavBar = () => {
                     </div>  
                     <Link to={"/checkout"}>
                         <div className='flex pr-3 pl-3'>
-                            <ShoppingCartIcon className='h-[48px]'/>
+                            <ShoppingCartIcon className='h-[48px]' />
+                            <div className='relative'>
+                                <div className='absolute right-[9px] font-bold m-2 text-orange-400'>
+                                    {cart}
+                                </div>
+                            </div>
                             <div className='mt-7 text-xs xl:text-sm font-bold'>Cart</div>
                         </div>
                     </Link>
